0206b0cd963f65ee5f92ad07fd405f4e794c04e6
Diego Rayo <diegofrayo@gmail.com>
2017-06-02 14:35:36 -0500

    Second version: We used a new template, added services and portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -1,127 +1,117 @@
 var browserSync = require('browser-sync').create();
 var cleanCSS = require('gulp-clean-css');
 var gulp = require('gulp');
-var header = require('gulp-header');
 var htmlmin = require('gulp-htmlmin');
 var less = require('gulp-less');
 var pkg = require('./package.json');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
-// Set the banner content
-var banner = ['/*!\n',
-   ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-   ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-   ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
-   ' */\n',
-   ''
-].join('');
 
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
-   return gulp.src(['less/landing.less'])
-      .pipe(less())
-      .pipe(header(banner, {
-         pkg: pkg
-      }))
-      .pipe(gulp.dest('css'))
-      .pipe(browserSync.reload({
-         stream: true
-      }));
+  return gulp.src(['less/landing.less'])
+    .pipe(less())
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-// Minify compiled CSS
-gulp.task('minify-css', ['less'], function() {
-   return gulp.src(['css/landing.css'])
-      .pipe(cleanCSS({
-         compatibility: 'ie8'
-      }))
-      .pipe(rename({
-         suffix: '.min'
-      }))
-      .pipe(gulp.dest('css'))
-      .pipe(browserSync.reload({
-         stream: true
-      }));
+
+// build compiled CSS
+gulp.task('build-css', ['less'], function() {
+  return gulp.src(['css/landing.css'])
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-// Minify JS
+
+// build JS
+gulp.task('build-js', function() {
+  return gulp.src(['js/landing.js'])
+    .pipe(gulp.dest('js'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+
+// build HTML
+gulp.task('build-html', function() {
+  return gulp.src('./index-dev.html')
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+
+// minify CSS
+gulp.task('minify-css', function() {
+  return gulp.src(['css/landing.css'])
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(gulp.dest('./build/css/'));
+});
+
+
+// minify JS
 gulp.task('minify-js', function() {
-   return gulp.src(['js/landing.js'])
-      .pipe(uglify())
-      .pipe(header(banner, {
-         pkg: pkg
-      }))
-      .pipe(rename({
-         suffix: '.min'
-      }))
-      .pipe(gulp.dest('js'))
-      .pipe(browserSync.reload({
-         stream: true
-      }));
+  return gulp.src(['js/landing.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
 });
 
-// Minify HTML
+
+// minify HTML
 gulp.task('minify-html', function() {
-   return gulp.src('./index-dev.html')
-      .pipe(rename('index.html'))
-      .pipe(htmlmin({
-         collapseBooleanAttributes: true,
-         collapseWhitespace: true,
-         removeComments: true,
-         removeEmptyAttributes: false,
-         removeRedundantAttributes: true
-      }))
-      .pipe(gulp.dest('./'))
-      .pipe(browserSync.reload({
-         stream: true
-      }));
+  return gulp.src('./index.html')
+    .pipe(htmlmin({
+      collapseBooleanAttributes: true,
+      collapseWhitespace: true,
+      removeComments: true,
+      removeEmptyAttributes: false,
+      removeRedundantAttributes: true
+    }))
+    .pipe(gulp.dest('./build'));
 });
 
-// Copy vendor libraries from /node_modules into /vendor
-gulp.task('copy', function() {
 
-   gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-      .pipe(gulp.dest('vendor/bootstrap'));
+gulp.task('copy-assets', function() {
+  gulp.src(['./img/**/*.png', './img/**/*.jpg', './img/**/*.ico', '!./img/backgrounds/original/*', '!./img/bg-patterns/original/*', '!./img/portfolio/original/*', '!./img/team/original/*', '!./img/tech-stack/original/*'])
+    .pipe(gulp.dest('./build/img'));
+});
 
-   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-      .pipe(gulp.dest('vendor/jquery'));
-
-   gulp.src(['node_modules/magnific-popup/dist/*'])
-      .pipe(gulp.dest('vendor/magnific-popup'));
-
-   gulp.src(['node_modules/scrollreveal/dist/*.js'])
-      .pipe(gulp.dest('vendor/scrollreveal'));
-
-   gulp.src([
-         'node_modules/font-awesome/**',
-         '!node_modules/font-awesome/**/*.map',
-         '!node_modules/font-awesome/.npmignore',
-         '!node_modules/font-awesome/*.txt',
-         '!node_modules/font-awesome/*.md',
-         '!node_modules/font-awesome/*.json'
-      ])
-      .pipe(gulp.dest('vendor/font-awesome'));
-})
 
 // Run everything
-gulp.task('default', ['less', 'minify-css', 'minify-js', 'minify-html', 'copy']);
+gulp.task('default', ['less', 'build-css', 'build-js', 'build-html']);
+
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
-   browserSync.init({
-      server: {
-         baseDir: ''
-      },
-   });
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  });
 });
 
+
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-html', 'minify-js'], function() {
-   gulp.watch('less/*.less', ['less']);
-   gulp.watch('css/*.css', ['minify-css']);
-   gulp.watch('js/*.js', ['minify-js']);
-   // Reloads the browser whenever HTML or JS files change
-   gulp.watch('index-dev.html', ['minify-html']);
-   gulp.watch('js/**/*.js', browserSync.reload);
+gulp.task('dev', ['browserSync', 'less', 'build-css', 'build-html', 'build-js'], function() {
+  gulp.watch('less/*.less', ['less']);
+  gulp.watch('css/*.css', ['build-css']);
+  gulp.watch('js/*.js', ['build-js']);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('index-dev.html', ['build-html']);
+  gulp.watch('js/**/*.js', browserSync.reload);
 });
+
+
+// Build tasks
+gulp.task('build', ['default', 'minify-css', 'minify-js', 'minify-html', 'copy-assets'], function() {});
--- js/landing.js
+++ js/landing.js
@@ -1,5 +1,4 @@
 (function($) {
-  "use strict"; // Start of use strict
 
   // jQuery for page scrolling feature - requires jQuery Easing plugin
   $('a.page-scroll').bind('click', function(event) {
@@ -28,78 +27,37 @@
     }
   })
 
-})(jQuery); // End of use strict
+})(jQuery);
 
 
-// // Contact Form Scripts
+// Contact Form Scripts
+$(function() {
 
-// $(function() {
+  $('#contact-form').submit(function(event) {
 
-//     $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-//         preventSubmit: true,
-//         submitError: function($form, event, errors) {
-//             // additional error messages or events
-//         },
-//         submitSuccess: function($form, event) {
-//             event.preventDefault(); // prevent default submit behaviour
-//             // get values from FORM
-//             var name = $("input#name").val();
-//             var email = $("input#email").val();
-//             var phone = $("input#phone").val();
-//             var message = $("textarea#message").val();
-//             var firstName = name; // For Success/Failure Message
-//             // Check for white space in name for Success/Fail message
-//             if (firstName.indexOf(' ') >= 0) {
-//                 firstName = name.split(' ').slice(0, -1).join(' ');
-//             }
-//             $.ajax({
-//                 url: "././mail/contact_me.php",
-//                 type: "POST",
-//                 data: {
-//                     name: name,
-//                     phone: phone,
-//                     email: email,
-//                     message: message
-//                 },
-//                 cache: false,
-//                 success: function() {
-//                     // Success message
-//                     $('#success').html("<div class='alert alert-success'>");
-//                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-//                         .append("</button>");
-//                     $('#success > .alert-success')
-//                         .append("<strong>Your message has been sent. </strong>");
-//                     $('#success > .alert-success')
-//                         .append('</div>');
+    event.preventDefault();
 
-//                     //clear all fields
-//                     $('#contactForm').trigger("reset");
-//                 },
-//                 error: function() {
-//                     // Fail message
-//                     $('#success').html("<div class='alert alert-danger'>");
-//                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-//                         .append("</button>");
-//                     $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-//                     $('#success > .alert-danger').append('</div>');
-//                     //clear all fields
-//                     $('#contactForm').trigger("reset");
-//                 },
-//             });
-//         },
-//         filter: function() {
-//             return $(this).is(":visible");
-//         },
-//     });
+    var name = $('input#name').val();
+    var email = $('input#email').val();
+    var phone = $('input#phone').val();
+    var message = $('textarea#message').val();
 
-//     $("a[data-toggle=\"tab\"]").click(function(e) {
-//         e.preventDefault();
-//         $(this).tab("show");
-//     });
-// });
+    var btnSendMessage = jQuery('#btn-send-message');
+    btnSendMessage.button('loading');
 
+    setTimeout(function() {
 
-// /*When clicking on Full hide fail/success boxes */
-// $('#name').focus(function() {
-//     $('#success').html('');
-// });
+      //clear all fields
+      btnSendMessage.button('reset');
+      $('#send-message-success-alert').removeClass('hidden').fadeIn(1000);
+
+      setTimeout(function() {
+        $('#contact-form').trigger('reset');
+        $('#send-message-success-alert').fadeOut(1000);
+      }, 5000);
+
+    }, 2000);
+
+  });
+
+});
